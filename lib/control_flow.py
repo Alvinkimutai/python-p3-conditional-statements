e0eb61bd39f2e13da54e03bb0be0c5bd5bac0734
Alvinkimutai <alvinchepkiyeng@gmail.com>
2024-08-31 18:31:16 +0300

    practices on if/elif and else

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -2,16 +2,50 @@
 
 def admin_login(username, password):
     # your code here
+    username = username.lower()
+    if username == "admin" and password == "12345":
+        return "Access granted"
+    else:
+        return "Access denied"
     pass
+print(admin_login("admin", "12345"))
 
 def hows_the_weather(temperature):
     # your code here
+    if temperature < 40:
+        return "It's brisk out there!"
+    elif temperature>= 40 and temperature<= 65:
+        return "It's a little chilly out there!"
+    elif temperature>65 and temperature <= 85:
+        return "It's perfect out there!"
+    else:
+        return "It's too dang hot out there!"
     pass
+print(hows_the_weather(99))
 
 def fizzbuzz(num):
-    # your code here
+     # your code here
+    if num%3 == 0 and num%5 != 0:
+        return "Fizz"
+    elif num%5 == 0 and num%3 !=0:
+        return "Buzz"
+    elif num%3 == 0 and num%5 == 0:
+        return "FizzBuzz"
+    else:
+        return num
     pass
-
+print(fizzbuzz(0))
 def calculator(operation, num1, num2):
     # your code here
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1 * num2
+    elif operation == "/":
+        return num1 / num2
+    else:
+        print("Invalid operation!")
     pass
+print(calculator(".", 12, 3))
